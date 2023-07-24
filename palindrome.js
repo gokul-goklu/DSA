a61e8bd5eb1f1edbc5d26500e52244742f7db01d
gokul-goklu <gokulshanmugam062@gmail.com>
2023-07-24 21:56:07 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+const palindromicString = (str) => {
+  str = str.toLowerCase().trim();
+  let j = str.length - 1,
+    c = 0;
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] != str[j--]) {
+      c = 1;
+      break;
+    }
+  }
+  if (c == 0) {
+    console.log("palindrome");
+  } else console.log("not palindrome");
+};
+
+let str = "Malayala";
+palindromicString(str);
